f22673f8ec30951d557ff5add8b337f499651601
linying1223 <993725274@qq.com>
2018-03-28 23:32:46 +0800

    首页效果完成

--- pages/cssbook/book.js
+++ pages/cssbook/book.js
@@ -2,7 +2,7 @@
 
 const util = require('../../utils/util.js')
 const imgPrefix = "https://integratedwall.oss-cn-beijing.aliyuncs.com/books/"
-// const iconPrefix = "https://integratedwall.oss-cn-beijing.aliyuncs.com/books/icon"
+const iconPrefix = "https://integratedwall.oss-cn-beijing.aliyuncs.com/books/icon/"
 const innerImgs = [1, 2, 3, 4, 50, 51, 101, 102, 152, 201, 202, 251, 252, 302, 352, 353, 387, 453, 454];
 const imgSuffix = ".jpg";
 var app = getApp();
@@ -11,15 +11,29 @@ var unreadImgs = [{ img: 1, y: 0, zidx: 3 }, { img: 2, y: 0, zidx: 2 }, { img: 3
 
 Page({
   data: {
-    logs: [],
-    unreadImgs: [],
-    imgPrefix: '',
-    imgSuffix: '',
-    iconPrefix: "https://integratedwall.oss-cn-beijing.aliyuncs.com/books/icon/",
+    unreadImgs: unreadImgs,
+    imgPrefix: imgPrefix,
+    imgSuffix: imgSuffix,
+    iconPrefix: iconPrefix,
     clientHeight: '',
     clientWidth: '',
-    rotateY: 0,
-    readIdx:0
+    readIdx: 0,
+    headTopAnimate: 'slideUp',
+    bottomAnimate: 'bottomSlideDown'
+  },
+  handletap: function () {
+    if (this.data.headTopAnimate == "slideDown") {
+      this.setData({
+        headTopAnimate: 'slideUp',
+        bottomAnimate: 'bottomSlideDown'
+      })
+    } else {
+      this.setData({
+        headTopAnimate: 'slideDown',
+        bottomAnimate: 'bottomSlideUp'
+      })
+    }
+
   },
   //handletouchtart+handletouchmove 判斷左滑右滑
   handletouchtart: function (e) {
@@ -27,10 +41,11 @@ Page({
     app.globalData.touch.lastY = e.touches[0].pageY
   },
   handletouchend: function (e) {
-    if ((app.globalData.touch.lastX - e.changedTouches[0].pageX) > 0){
+    var move = (app.globalData.touch.lastX - e.changedTouches[0].pageX)
+    if (move > 2) {
       //左滑
       //console.log("left=" + this.data.readIdx)
-      if (this.data.readIdx <= unreadImgs.length-1){
+      if (this.data.readIdx <= unreadImgs.length - 1) {
         //TODO skill 這裡更新數據屬性 也可以實現類似操作demo的效果
         unreadImgs[this.data.readIdx].y = -90;
         //z-index=數組長度-元素下標位置
@@ -40,43 +55,30 @@ Page({
         });
 
         this.data.readIdx += 1;
-      // console.log("left="+this.data.readIdx)
-      // console.log(unreadImgs)
+        // console.log("left="+this.data.readIdx)
+        // console.log(unreadImgs)
       }
-     }else{
+    } else if (move < -2) {
       // console.log("right 1=" + this.data.readIdx)
       // console.log(unreadImgs)
-      if (this.data.readIdx>=1){
-        unreadImgs[this.data.readIdx-1].y = 0;
+      if (this.data.readIdx >= 1) {
+        unreadImgs[this.data.readIdx - 1].y = 0;
         this.setData({
           unreadImgs: unreadImgs
         });
         this.data.readIdx -= 1;
       }
-     
-      //右滑
-      // this.setData({
-      //   rotateY: 0
-      // });
     }
-       
+
   },
   onLoad: function () {
     var that = this;
     wx.getSystemInfo({
       success: function (res) {
-
         that.setData({
-          unreadImgs: unreadImgs,
-          imgPrefix: imgPrefix,
-          imgSuffix: imgSuffix,
           clientHeight: res.windowHeight,
-          clientWidth: res.windowWidth,
-          logs: (wx.getStorageSync('logs') || []).map(log => {
-            return util.formatTime(new Date(log))
-          })
+          clientWidth: res.windowWidth
         })
-
       }
     });
 
