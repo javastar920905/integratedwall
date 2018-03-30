be61e8fbcb29d13655387f301bbe8e9363d978aa
linying1223 <993725274@qq.com>
2018-03-30 23:22:36 +0800

    loading 优化，懒加载优化，头部底部样式调整

--- pages/cssbook/book.js
+++ pages/cssbook/book.js
@@ -7,10 +7,10 @@ const imgSuffix = ".jpg";
 
 // 模拟所有图片队列
 const innerImgs = [1, 2, 3, 4, 50, 51, 101, 102, 152, 201, 202, 251, 252, 302, 352, 353, 387, 453, 454
-   ,1, 2, 3, 4, 50, 51, 101, 102, 152, 201, 202, 251, 252, 302, 352, 353, 387, 453, 454
-  ,1, 2, 3, 4, 50, 51, 101, 102, 152, 201, 202, 251, 252, 302, 352, 353, 387, 453, 454
   , 1, 2, 3, 4, 50, 51, 101, 102, 152, 201, 202, 251, 252, 302, 352, 353, 387, 453, 454
-  ];
+  , 1, 2, 3, 4, 50, 51, 101, 102, 152, 201, 202, 251, 252, 302, 352, 353, 387, 453, 454
+  , 1, 2, 3, 4, 50, 51, 101, 102, 152, 201, 202, 251, 252, 302, 352, 353, 387, 453, 454
+];
 /**未读图片队列 圖片名,當前y軸角度,z-index=(數組長度-元素下標位置 **/
 var unreadImgs = [{ img: 1, y: 0, zidx: 3 }, { img: 2, y: 0, zidx: 2 }, { img: 3, y: 1, zidx: 1 }];
 
@@ -60,6 +60,7 @@ Page({
   },
   handletouchend: function (e) {
     var move = (app.globalData.touch.lastX - e.changedTouches[0].pageX)
+    //误差值 是避免点击时，触发滑动效果
     if (move > 2) {
       //左滑
       //console.log("left=" + this.data.readIdx)
@@ -77,8 +78,6 @@ Page({
         // console.log(unreadImgs)
       }
     } else if (move < -2) {
-      // console.log("right 1=" + this.data.readIdx)
-      // console.log(unreadImgs)
       if (this.data.readIdx >= 1) {
         unreadImgs[this.data.readIdx - 1].y = 0;
         this.setData({
@@ -97,39 +96,34 @@ Page({
     this.lazyLoadImg()
   },
   handlescroll: function (e) {
-    //console.log(e.detail)
-    /** deltaX:0,deltaY:-12scrollHeight:1776 (固定值)，scrollLeft:0，scrollTop:213（下滑变大）scrollWidth:320 （固定值）**/
     let mod = e.detail.scrollTop % 200;
-    // console.log(mod)
-    if (mod > 150) {
+    if (mod > 100) {
       this.lazyLoadImg();
     }
   },
   lazyLoadImg: function () {
     let bufferImgs = innerImgs.slice(this.data.loadedImgStart, this.data.loadedImgEnd);
-    console.log(bufferImgs);
-    if(bufferImgs.length>0){
+    //图片没有加载完才继续加载
+    if (bufferImgs.length > 0) {
       for (var i = 0; i < bufferImgs.length; i++) {
-        if (bufferImgs[i]!=null){
+        if (bufferImgs[i] != null) {
           lazyImgs.splice(this.data.loadedImgStart, 1, bufferImgs[i]);
-          this.data.loadedImgStart ++;
-        }else{
+          this.data.loadedImgStart++;
+        } else {
           break;
         }
       }
-    
-      this.data.loadedImgEnd = this.data.loadedImgEnd * 2;
 
-      console.log("start"+this.data.loadedImgStart + " end " + this.data.loadedImgEnd);
-       console.log(lazyImgs);
+      //每次滚动加载3张图
+      this.data.loadedImgEnd = this.data.loadedImgEnd + 3;
+
+      // console.log("start"+this.data.loadedImgStart + " end " + this.data.loadedImgEnd);
+      //  console.log(lazyImgs);
       this.setData({
         lazyImgs: lazyImgs
       });
-    }else{
-      console.log("所有圖片加載完畢")
-      console.log(bufferImgs);
     }
-    
+
   },
   onLoad: function () {
     var that = this;
